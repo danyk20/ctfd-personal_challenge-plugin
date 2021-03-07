d679f5dd9fadbd10590badfad891af534a61dd38
Daniel Košč <xkosc@fi.muni.cz>
2021-03-07 23:35:51 +0100

    populate test

--- personal_challenges/tests/populate.py
+++ personal_challenges/tests/populate.py
@@ -1,3 +1,4 @@
+import random
 from CTFd import create_app
 from CTFd.models import (Challenges, Users, Admins)
 from CTFd.plugins.personal_challenges import IndividualFlag
@@ -14,11 +15,11 @@ def create_individual_flag(player_id=1, challenge_id=1):
     with app.app_context():
         db = app.db
         f = IndividualFlag(
-            user_id=player_id,
-            challenge_id=challenge_id,
+            user_id=player_id + 1,
+            challenge_id=challenge_id + 1,
             content="case_sensitive",
             type="individual",
-            data=names[id] + str(challenge_id)
+            data=names[player_id] + str(challenge_id)
         )
         db.session.add(f)
         db.session.commit()
@@ -46,7 +47,7 @@ def create_admin():
         adm = Admins(
             name="admin",
             password="admin",
-            email="admin@muni.cz",
+            email="admin@muni.cz" + str(random.randint(0,10000)),
         )
         db.session.add(adm)
         db.session.commit()
@@ -59,7 +60,7 @@ def create_player(id=1):
         pl = Users(
             name=names[id],
             password="password",
-            email=names[id] + "@muni.cz",
+            email=names[id] + "@muni.cz" + str(random.randint(0,10000)),
         )
         db.session.add(pl)
         db.session.commit()
@@ -67,10 +68,16 @@ def create_player(id=1):
 
 
 def generate():
+    print("Generating admin")
     create_admin()
     for p in range(players):
+        print("Generating player " + str(p))
         create_player(p)
     for c in range(challenges):
+        print("Generating challenge " + str(c))
         create_challenge(c)
         for p in range(players):
+            print("Generating flag for player " + str(p) + " in challenge " + str(c))
             create_individual_flag(p, c)
+
+generate()
